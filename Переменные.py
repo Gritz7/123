3a1ff8d690891b61f4d5becb23a7d608afc238be
Gritz7 <makarevichsa77@gmail.com>
2024-06-30 15:13:44 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+hw_count = 12
+hw_hours = 1.5
+course_name = "Python"
+average_time_1task = hw_hours/hw_count
+print(f"Курс:{course_name}, всего задач:{hw_count}, затрачено часов:{hw_hours}, среднее время выполнения:{average_time_1task}.")
